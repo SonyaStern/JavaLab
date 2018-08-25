b8c2a54e90af3698b3317e912d73ee5938b1119b
SonyaStern <32080819+SonyaStern@users.noreply.github.com>
2018-08-25 19:17:47 +0300

    Add files via upload

---
+++ Spring/src/main/java/quoters/TerminatorQuoter.java
@@ -0,0 +1,17 @@
+package quoters;
+
+public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void sayQuote() {
+        System.out.println("message = " + message);
+    }
+}
